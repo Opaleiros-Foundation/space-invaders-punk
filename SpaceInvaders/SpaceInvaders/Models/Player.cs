225d249dd587ff0a7e7a0797bf2f2de9e2f78440
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-01 01:50:58 -0300

    refactor: convert Player record to partial class with observable properties

--- SpaceInvaders/SpaceInvaders/Models/Player.cs
+++ SpaceInvaders/SpaceInvaders/Models/Player.cs
@@ -1,3 +1,26 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace SpaceInvaders.Models;
 
-public record Player(string Name);
+public partial class Player : ObservableObject
+{
+    [ObservableProperty]
+    private string _name;
+
+    [ObservableProperty]
+    private double _x;
+
+    [ObservableProperty]
+    private double _y;
+
+    [ObservableProperty]
+    private int _score;
+
+    public Player(string name)
+    {
+        _name = name;
+        _x = 0;
+        _y = 0;
+        _score = 0;
+    }
+}
