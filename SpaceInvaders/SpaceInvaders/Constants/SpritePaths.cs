d2eae74f57bf546e6e8e9a99ccd5802b6d120d71
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-01 02:47:42 -0300

    feat: add projectile sprite path

--- SpaceInvaders/SpaceInvaders/Constants/SpritePaths.cs
+++ SpaceInvaders/SpaceInvaders/Constants/SpritePaths.cs
@@ -6,4 +6,5 @@ public static class SpritePaths
     public const string AlienType1 = "ms-appx:///Assets/sprites/aliens/alien1/type1.svg";
     public const string AlienType2 = "ms-appx:///Assets/sprites/aliens/alien2/type2.svg";
     public const string AlienType3 = "ms-appx:///Assets/sprites/aliens/alien3/type3.svg";
+    public const string Projectile = "ms-appx:///Assets/sprites/projectile/projectile.png";
 }
