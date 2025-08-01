51159f2cb0660ff64eef87941eaf0f3c8c40fb72
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-01 02:30:17 -0300

    feat: add SpritePaths class for managing sprite asset paths

---
+++ SpaceInvaders/SpaceInvaders/Constants/SpritePaths.cs
@@ -0,0 +1,9 @@
+namespace SpaceInvaders.Constants;
+
+public static class SpritePaths
+{
+    public const string Player = "ms-appx:///Assets/sprites/player/index.png";
+    public const string AlienType1 = "ms-appx:///Assets/sprites/aliens/alien1/type1.svg";
+    public const string AlienType2 = "ms-appx:///Assets/sprites/aliens/alien2/type2.svg";
+    public const string AlienType3 = "ms-appx:///Assets/sprites/aliens/alien3/type3.svg";
+}
