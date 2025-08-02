26a53e21ecb42a832969cce921a6bae5e61b7325
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-01 21:32:17 -0300

    feat: implement game start page logic and view

--- SpaceInvaders/SpaceInvaders/Presentation/GameStartPage.xaml.cs
+++ SpaceInvaders/SpaceInvaders/Presentation/GameStartPage.xaml.cs
@@ -12,19 +12,81 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-
-
-using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.System;
+using Microsoft.UI.Dispatching;
 
 namespace SpaceInvaders.Presentation;
 
 public sealed partial class GameStartPage : Page
 {
+    private List<Image> alienImages = new List<Image>();
+
     public GameStartPage()
     {
         this.InitializeComponent();
         this.Loaded += GameStartPage_Loaded;
+        this.DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+    {
+        if (DataContext is GameStartPageViewModel viewModel)
+        {
+            CreateAlienImages(viewModel);
+            viewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+    }
+
+    private void CreateAlienImages(GameStartPageViewModel viewModel)
+    {
+        GameCanvas.Children.Clear(); 
+        alienImages.Clear();
+
+        foreach (var alien in viewModel.Aliens)
+        {
+            var image = new Image
+            {
+                Width = 64,
+                Height = 64,
+                Source = new BitmapImage(new Uri(alien.SpritePath))
+            };
+
+            Canvas.SetLeft(image, alien.X);
+            Canvas.SetTop(image, alien.Y);
+
+            GameCanvas.Children.Add(image);
+            alienImages.Add(image);
+
+            // Subscribe to alien's position changes
+            alien.PropertyChanged += (s, e) =>
+            {
+                this.DispatcherQueue.TryEnqueue(() =>
+                {
+                    if (e.PropertyName == nameof(alien.X))
+                    {
+                        Canvas.SetLeft(image, alien.X);
+                    }
+                    if (e.PropertyName == nameof(alien.Y))
+                    {
+                        Canvas.SetTop(image, alien.Y);
+                    }
+                    GameCanvas.InvalidateMeasure();
+                    GameCanvas.UpdateLayout();
+                });
+            };
+        }
+    }
+
+    private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(GameStartPageViewModel.Aliens))
+        {
+            if (sender is GameStartPageViewModel viewModel)
+            {
+                CreateAlienImages(viewModel);
+            }
+        }
     }
 
     private void GameStartPage_Loaded(object sender, RoutedEventArgs e)
