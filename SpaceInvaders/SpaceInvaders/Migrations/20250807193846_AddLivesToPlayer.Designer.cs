52e1a813a0d73cf161002701f0f3bd611016c9ff
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-07 16:53:44 -0300

    feat: Add lives to player model and update database
    This commit introduces the 'Lives' property to the Player model and includes the corresponding database migration to add the 'Lives' column to the 'Players' table.

--- SpaceInvaders/SpaceInvaders/Migrations/20250807193846_AddLivesToPlayer.Designer.cs
+++ SpaceInvaders/SpaceInvaders/Migrations/20250807193846_AddLivesToPlayer.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SpaceInvaders.Data;
@@ -11,9 +12,11 @@ using SpaceInvaders.Data;
 namespace SpaceInvaders.Migrations
 {
     [DbContext(typeof(SpaceInvadersDbContext))]
-    partial class SpaceInvadersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250807193846_AddLivesToPlayer")]
+    partial class AddLivesToPlayer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SpaceInvaders/SpaceInvaders/Migrations/20250807193846_AddLivesToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpaceInvaders.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLivesToPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Lives",
+                table: "Players",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lives",
+                table: "Players");
+        }
+    }
+}
