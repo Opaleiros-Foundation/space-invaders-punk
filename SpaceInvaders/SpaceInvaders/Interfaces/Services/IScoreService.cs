a484f950f0e5974b5ea5f758b4fb04edca3ed7a4
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-13 00:27:40 -0300

    Docs: Add XML documentation to Interfaces

--- SpaceInvaders/SpaceInvaders/Interfaces/Services/IScoreService.cs
+++ SpaceInvaders/SpaceInvaders/Interfaces/Services/IScoreService.cs
@@ -4,9 +4,26 @@ using SpaceInvaders.Models;
 
 namespace SpaceInvaders.Interfaces.Services;
 
+/// <summary>
+/// Defines the methods for the score management service.
+/// </summary>
 public interface IScoreService
 {
+    /// <summary>
+    /// Asynchronously adds a new score.
+    /// </summary>
+    /// <param name="score">The Score object to be added.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the added score.</returns>
     Task<Score> AddScoreAsync(Score score);
+    /// <summary>
+    /// Asynchronously retrieves the top scores.
+    /// </summary>
+    /// <param name="count">The number of top scores to return.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a list of the top scores.</returns>
     Task<List<Score>> GetTopScoresAsync(int count);
+    /// <summary>
+    /// Asynchronously retrieves all scores.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation, containing a list of all scores.</returns>
     Task<List<Score>> GetAllScoresAsync();
 }
--- SpaceInvaders/SpaceInvaders/Interfaces/Services/ISoundService.cs
+++ SpaceInvaders/SpaceInvaders/Interfaces/Services/ISoundService.cs
@@ -1,6 +1,13 @@
 namespace SpaceInvaders.Interfaces.Services;
 
+/// <summary>
+/// Defines the methods for the sound playback service.
+/// </summary>
 public interface ISoundService
 {
+    /// <summary>
+    /// Plays a sound from the specified path.
+    /// </summary>
+    /// <param name="soundPath">The path to the sound file to be played.</param>
     void PlaySound(string soundPath);
 }
